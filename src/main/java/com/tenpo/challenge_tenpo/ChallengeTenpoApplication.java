e1755fcd2bfa65041d0c5077a92903635f61608e
Carlos Gonzales Ramos <rcarlosgonzales98@gmail.com>
2025-03-04 21:36:15 -0500

    Creación del proyecto

---
+++ src/main/java/com/tenpo/challenge_tenpo/ChallengeTenpoApplication.java
@@ -0,0 +1,13 @@
+package com.tenpo.challenge_tenpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeTenpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeTenpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenpo/challenge_tenpo/ChallengeTenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenpo.challenge_tenpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeTenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
